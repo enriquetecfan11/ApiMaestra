4b20ab40a046cab46e6a54324cc00d7e38cfc19c
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2024-04-24 11:00:52 +0200

    Update ScriptContainer.sh to improve container management

--- ScriptContainer.sh
+++ ScriptContainer.sh
@@ -9,16 +9,26 @@ read opcion
 
 case $opcion in
   1)
-    echo "Starting container..."
+    echo "Create image for the container..."
     # Create a container from the image
     docker build -t apimaestra .
+
+    sleep 5
+    
     # Run the container
+    echo "Running container..."
     docker run  -d -p 8080:8000 --name apimaestra apimaestra
+
+    sleep 2
+    echo "Container running..."
+    docker ps -a
     ;;
   2)
     echo "Stoping and remove container ..."
     # Stop the container
     docker stop apimaestra
+    sleep 2
+    echo "Removing image ..."
     # Remove image
     docker rm apimaestra
     ;;
